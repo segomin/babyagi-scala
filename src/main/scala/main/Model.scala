ad9c803a85ab797039e31768e7ddb7bda0009340
민상호 <shmin@seowoninfo.com>
2023-05-08 00:34:50 +0900

    imple openai api

--- src/main/scala/main/Model.scala
+++ src/main/scala/main/Model.scala
@@ -55,3 +55,45 @@ object QueryItem {
   implicit val rw: ReadWriter[QueryItem] = macroRW
 }
 
+/**
+  * OpenAI Create embeddings
+  *  https://platform.openai.com/docs/api-reference/embeddings/create
+  */
+case class CreateEmbeddingRequest(input: String, model: String = "text-embedding-ada-002")
+object CreateEmbeddingRequest {
+  implicit val rw: ReadWriter[CreateEmbeddingRequest] = macroRW
+}
+
+case class CreateEmbeddingResponse(`object`: String, data: List[Embedding], model: String)
+case class Embedding(`object`: String, embedding: List[Double], index: Int)
+object CreateEmbeddingResponse {
+  implicit val rw: ReadWriter[CreateEmbeddingResponse] = macroRW
+}
+object Embedding {
+  implicit val rw: ReadWriter[Embedding] = macroRW
+}
+
+/**
+  * OpenAI Create Completion
+  *  https://platform.openai.com/docs/api-reference/completions/create
+  */
+case class CreateCompletionRequest(model: String = "text-davinci-003",
+                                   prompt: String,
+                                   max_tokens: Int = 16,
+                                   temperature: Double = 1,
+                                   top_p: Int = 1,
+                                   n: Int = 1,
+                                   stream: Boolean = false,
+                                   frequency_penalty: Double = 0.0,
+                                   presence_penalty: Double = 0.0)
+case class CreateCompletionResponse(id: String, `object`: String, created: Long, model: String, choices: List[Choice])
+case class Choice(text: String, index: Int, finish_reason: String)
+object CreateCompletionRequest {
+  implicit val rw: ReadWriter[CreateCompletionRequest] = macroRW
+}
+object CreateCompletionResponse {
+  implicit val rw: ReadWriter[CreateCompletionResponse] = macroRW
+}
+object Choice {
+  implicit val rw: ReadWriter[Choice] = macroRW
+}
